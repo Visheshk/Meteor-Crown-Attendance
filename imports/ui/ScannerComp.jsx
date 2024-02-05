5ae45b19dbe3dba0a531da6babbe8107d22b47aa
Vishesh Kumar <visheshkay@gmail.com>
2024-02-04 20:40:51 -0600

    getting room page and log addition to work

--- imports/ui/ScannerComp.jsx
+++ imports/ui/ScannerComp.jsx
@@ -1,26 +1,33 @@
 import React, { useState } from 'react';
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
-export const ScannerComp = () => {
+export const ScannerComp = ({spotUser}) => {
 	const [camData, setCamData] = useState(0);
 	const [stopStream, setStopStream] = useState(true);
 	const [codeVisitor, setCodeVisitor] = useState({});
+	// console.log(spotUser);
 
 	const toggleQrReader = () => {
-	    // Stop the QR Reader stream (fixes issue where the browser freezes when closing the modal) and then dismiss the modal one tick later
-	    setStopStream(!stopStream);
-	    console.log(stopStream);
-	    // setTimeout(() => closeModal(), 0);
-	  }
+    // Stop the QR Reader stream (fixes issue where the browser freezes when closing the modal) and then dismiss the modal one tick later
+    setStopStream(!stopStream);
+    console.log(stopStream);
+    // setTimeout(() => closeModal(), 0);
+  }
 
 	  async function checkBarcode (barcode) {
+	  	console.log(barcode);
 	    Meteor.call('visitors.findByBarcode', barcode, function (err, res) {
 	      if (err) {
 	        console.log(err);
 	      }
 	      else {
-	        console.log(res);
+	        // console.log(res);
+	        // console.log("setting code visitor");
 	        setCodeVisitor(res);
+	        // if (this.props){
+	        	// console.log(this.props);
+		        spotUser(barcode);
+	        // }
 	        return res;
 	      }
 	    });
@@ -40,7 +47,7 @@ export const ScannerComp = () => {
         />
         </div>
         <div className="barcode-column">
-          <h3>{camData} {JSON.stringify(codeVisitor)} </h3>
+          <h3>{JSON.stringify(codeVisitor)} </h3>
           
         </div>
       </div>
